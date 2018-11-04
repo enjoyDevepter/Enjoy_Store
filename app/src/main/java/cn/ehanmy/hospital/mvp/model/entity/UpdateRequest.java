c83dffd7665e861d98af5a089eed69bae1364931
guomin <guomin@mapbar.com>
2018-11-04 23:09:20 +0800

    chg 更新升级接口，新增友盟统计

--- app/src/main/java/cn/ehanmy/hospital/mvp/model/entity/UpdateRequest.java
+++ app/src/main/java/cn/ehanmy/hospital/mvp/model/entity/UpdateRequest.java
@@ -4,7 +4,7 @@ package cn.ehanmy.hospital.mvp.model.entity;
 import cn.ehanmy.hospital.mvp.model.entity.request.BaseRequest;
 
 public class UpdateRequest extends BaseRequest {
-    private final int cmd = 911;
+    private final int cmd = 10509;
     private String type;
 
     public int getCmd() {
